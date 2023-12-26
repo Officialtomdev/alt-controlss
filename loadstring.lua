f115781779f33af96a1cc2727d3e61e80d89a21e
officialtomdev <tomgifgaq@gmail.com>
2023-12-26 11:57:02 +0000

    neww

--- loadstring.lua
+++ loadstring.lua
@@ -1,37 +1,20 @@
-getgenv().Security = {
-    ["Security"] = {
-        ["Key"] = "", -- security given in this
-        ["DiscordUserName"] = "", -- should already be filled in. leave it as that.
-    }
-}
-
 getgenv().Settings = {
     ["HostSettings"] = {
-        ["Controller"] = 12345,
-        ["Hostfps"] = 30, -- set to null for unlimited
-        ["ControllerGui"] = true,
+        ["Controller"] = 12345, -- host must be inside of group
     },
     ["Settings"] = {
-        ["prefix"] = "!"
+        ["prefix"] = "."
     },
-    ["CrewSettings"] = { -- alt must already be in the group id
-        ["CrewID"] = 1,
-        ["JoinCrew"] = true
-    },
-    ["AdSettings"] = {
-        ["Message"] = "" -- put a message in here ( you can always change with using setad command)
-    }
 
 }
 
-getgenv().AltSettings {
+getgenv().AltSettings =  {
     ["AltSettings"] = {
         ["AltFps"] = 5, -- set to null for unlimited
         ["Alts"] = { -- and so on down below, make sure to put a comma at end and max is 38 alts
             Alt1 = 12345,
             Alt2 = 12345,
             Alt3 = 12345,
-            Alt4 = 12345
         }
     }
 }
@@ -39,3 +22,6 @@ getgenv().AltSettings {
 
 
 ------------- loader ----- dont change below.
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Officialtomdev/alt-controlss/main/cusmos-altcontrol/free/free.lua", true))()
